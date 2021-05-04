6c15d7a3837e8fec2f647cc91113f527b7ba71a1
qiqf <723469354@qq.com>
2021-05-04 20:57:01 +0800

    style: 删除一些注释

--- src/curry.ts
+++ src/curry.ts
@@ -1,8 +1,6 @@
 const curry = (...args: [Function, ...unknown[]]): Function => {
   const [func, ...surplusArgs] = args
-  // 这个函数的返回值是一个联合类型
-  // func 函数执行的返回值 | curry
-  return function(...args: Array<unknown>) {
+  return function(...args: unknown[]) {
     const combineArgs = [...surplusArgs, ...args]
     if (combineArgs.length >= func.length) {
       return func.apply(null, combineArgs)
