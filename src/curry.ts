7b8a3a5dac4f0d99a62c8b5fb06755d607cf736e
qiqf <723469354@qq.com>
2021-05-06 23:38:03 +0800

    feat(curry.ts): 对 curry 函数重构, 增强 curry 类型安全

--- src/curry.ts
+++ src/curry.ts
@@ -1,12 +1,20 @@
-const curry = (...args: [Function, ...unknown[]]): Function => {
-  const [func, ...surplusArgs] = args
-  return function(...args: unknown[]) {
-    const combineArgs = [...surplusArgs, ...args]
-    if (combineArgs.length >= func.length) {
-      return func.apply(null, combineArgs)
-    } else {
-      return curry(func, ...combineArgs)
-    }
+/**
+ * 使用泛型别名为 curry 函数定义函数类型字面量
+ */
+type Curry<T = unknown, R = unknown> = (
+  fn: (...args: T[]) => R,
+  ...args: T[]
+) => (...value: T[]) => R | Curry<T, R>
+
+/**
+ * 具体实现
+ * @param fn
+ * @param args
+ */
+const curry: Curry = (fn, ...args) => {
+  return (...value) => {
+    const combineArgs = [...args, ...value]
+    return combineArgs.length >= fn.length ? fn.apply(null, combineArgs) : curry(fn, ...combineArgs)
   }
 }
 
