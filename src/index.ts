14cfda70ecd412201d27c46f2d604514c594b470
qiqf <723469354@qq.com>
2021-05-07 00:00:17 +0800

    feat: 移除注释, 调整文件目录

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-import curry from './curry'
+type Curry<T = any, R = any> = (
+  fn: (...args: T[]) => R,
+  ...args: T[]
+) => (...value: T[]) => R | Curry<T, R>
+
+const curry: Curry = (fn, ...args) => {
+  return (...value) => {
+    const c = [...args, ...value]
+    return c.length >= fn.length ? fn(...c) : curry(fn, ...c)
+  }
+}
 
 export default curry
